42d481a23ae2f22210c99691b609e02e188126b2
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2025-08-02 14:12:17 +0200

    Code tree

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -31,15 +31,27 @@ public class Node
         }
     }
 
+
+    // TODO Start Problem 2
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+
+        if (value < Data)
+            return Left != null && Left.Contains(value);
+
+        return Right != null && Right.Contains(value);
     }
 
-    public int GetHeight()
-    {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
-    }
+    // TODO Start Problem 4
+    // Replace this line with the correct return statement(s)
+        public int GetHeight()
+{
+    int leftHeight = Left?.GetHeight() ?? 0;
+    int rightHeight = Right?.GetHeight() ?? 0;
+
+    return 1 + Math.Max(leftHeight, rightHeight);
 }
+
+    }
